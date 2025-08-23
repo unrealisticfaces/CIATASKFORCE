2362408adaf538d8402f21e31a5c8b1b5d47a426
ProjectX <144492715+unrealisticfaces@users.noreply.github.com>
2025-08-23 14:03:40 +0800

    fixed ofw toggle

--- js/register.js
+++ js/register.js
@@ -7,14 +7,14 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDA_q4-NEMJij-ojfQIahQRFov1n6p7qNM",
-  authDomain: "cia-bayanihan-app.firebaseapp.com",
-  databaseURL: "https://cia-bayanihan-app-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "cia-bayanihan-app",
-  storageBucket: "cia-bayanihan-app.appspot.com",
-  messagingSenderId: "35363747720",
-  appId: "1:35363747720:web:23840cad1a7f8f3f442c3d",
-  measurementId: "G-8BGFGETSKD"
+  apiKey: "AIzaSyDA_q4-NEMJij-ojfQIahQRFov1n6p7qNM",
+  authDomain: "cia-bayanihan-app.firebaseapp.com",
+  databaseURL: "https://cia-bayanihan-app-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cia-bayanihan-app",
+  storageBucket: "cia-bayanihan-app.appspot.com",
+  messagingSenderId: "35363747720",
+  appId: "1:35363747720:web:23840cad1a7f8f3f442c3d",
+  measurementId: "G-8BGFGETSKD"
 };
 
 // Your Supabase credentials
